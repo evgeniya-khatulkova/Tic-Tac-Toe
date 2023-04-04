6c81136b23df25cfedc43042cef99cb79c1aee17
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-04-04 21:40:29 +0200

    create player class

--- game.rb
+++ game.rb
@@ -1,7 +1,22 @@
+class Player
+
+  attr_reader :name, :result
+
+  def initialize(name)
+    @name = name
+    @result = []
+  end
+end
+
 puts "Let's play Tic Tac Toe. This game requires two players. Please enter name of the first player."
-first_player_name = gets.chomp
+first_players_name = gets.chomp
 puts "Thank you. Now, please, enter name of the second player. "
-second_player_name = gets.chomp
+second_players_name = gets.chomp
+
+first_player = Player.new(first_players_name)
+second_player = Player.new(second_players_name)
+
+puts "Hello, #{first_player.name} and #{second_players_name}"
 
 grid = " 1 | 2 | 2
 ---+---+---"
