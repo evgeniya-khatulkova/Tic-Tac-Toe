1f17fb63b83bffb5ae21d77071113aa0bf962e30
evgeniyakhatulkova <hatulkova.ev@gmail.com>
2023-04-06 20:07:50 +0200

    style and add play again logic

--- game.rb
+++ game.rb
@@ -1,7 +1,6 @@
-WINNING_COMBINATION = [[1,2,3], [1,4,7], [7,8,9], [4,5,6], [2,5,8], [1,5,9], [3,5,7]]
+WINNING_COMBINATION = [[1, 2, 3], [1, 4, 7], [7, 8, 9], [4, 5, 6], [2, 5, 8], [1, 5, 9], [3, 5, 7]]
 
 class Player
-
   attr_reader :name, :result
   attr_accessor :winner
 
@@ -20,18 +19,21 @@ class Player
   end
 end
 
+grid_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-grid_array = [1,2,3,4,5,6,7,8,9]
+def grid_builder(number)
+  print " #{number}"
+  puts
+  print '---+---+---'
+  puts
+end
 
 def creating_grid(grid_array)
   puts
   grid_array.each_with_index do |number, index|
     index_new = index + 1
     if index_new % 3 == 0 && index_new != 9
-      print " #{number}"
-      puts
-      print "---+---+---"
-      puts
+      grid_builder(number)
     elsif index_new == 9
       print " #{number}"
     else
@@ -44,15 +46,14 @@ end
 puts "  Let's play Tic Tac Toe. This game requires two players. Please enter name of the first player."
 first_players_name = gets.chomp
 puts
-puts "Thank you. Now, please, enter name of the second player. "
+puts 'Thank you. Now, please, enter name of the second player.'
 second_players_name = gets.chomp
 puts
 
 first_player = Player.new(first_players_name)
 second_player = Player.new(second_players_name)
 
-
-puts "#{first_player.name} is playing with X and #{second_player.name} is playing with O. "
+puts "#{first_player.name} is playing with - X and #{second_player.name} is playing with - O. "
 puts
 
 creating_grid(grid_array)
@@ -60,17 +61,17 @@ creating_grid(grid_array)
 while second_player.winner != true
   puts
   puts "#{first_player.name}, it is your turn to play, choose the position on the grid"
-    choise = gets.chomp.to_i
+  choise = gets.chomp.to_i
   until grid_array.include?(choise)
-          puts "Choose another position"
-          choise = gets.chomp.to_i
-    end
-      first_player.result << choise
-      grid_array[choise - 1] = "X"
-if WINNING_COMBINATION.include?(first_player.result.sort)
-  first_player.you_won!
-  super_winner = second_player
-end
+    puts 'Choose another position'
+    choise = gets.chomp.to_i
+  end
+  first_player.result << choise
+  grid_array[choise - 1] = 'X'
+  if WINNING_COMBINATION.include?(first_player.result.sort)
+    first_player.you_won!
+    super_winner = second_player
+  end
 
   creating_grid(grid_array)
   puts
@@ -80,18 +81,26 @@ end
   puts "#{second_player.name}, it is your turn to play, choose the position on the grid"
   choise = gets.chomp.to_i
   until grid_array.include?(choise)
-          puts "Choose another position"
-          choise = gets.chomp.to_i
-    end
-      second_player.result << choise
-      grid_array[choise - 1] = "O"
-   if WINNING_COMBINATION.include?(second_player.result.sort)
-      second_player.you_won!
-      super_winner = second_player
-   end
+    puts 'Choose another position'
+    choise = gets.chomp.to_i
+  end
+  second_player.result << choise
+  grid_array[choise - 1] = 'O'
+  if WINNING_COMBINATION.include?(second_player.result.sort)
+    second_player.you_won!
+    super_winner = second_player
+  end
 
   creating_grid(grid_array)
   puts
 end
 
 puts "Congrats #{super_winner.name}. You won the game"
+
+puts 'Do you want to play again. y/n?'
+play_again = gets.chomp.downcase
+until play_again.downcase == 'y' || play_again.downcase == 'n'
+  puts 'y stands for yes, n for now'
+  play_again = gets.chomp
+end
+creating_grid(grid_array) if play_again == 'y'
